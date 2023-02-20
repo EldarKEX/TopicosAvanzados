fad33b758841b96fc376b7fb4e2dfe686d66c716
Eldar <Eldar@DESKTOP-CTIDNPU>
2023-02-19 20:07:52 -0700

    Se han agregado funcionalidades al archivo de registro

--- Activador/Activador/Form1.cs
+++ Activador/Activador/Form1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using global::System.ComponentModel;
+using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -20,26 +20,22 @@ namespace Activador
 
         string key = "b14ca9888a5e4233aace2ea2615a1916";
 
-        /*
-             s = textBox1.Text;
-            s = Encriptacion.EncryptString(key, s);
-            MessageBox.Show(s);
-
-
-        MessageBox.Show(Encriptacion.DecryptString(key, s));
-
-
-        */
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
+            Encriptacion A = new Encriptacion();
+            string s = A.EncryptString(key, textBox1.Text);
+            Registro B = new Registro();
+            B.CreateRegister();
+            B.ReadSubKey(s);
         }
 
 
 
         private void button2_Click(object sender, EventArgs e)
         {
+            Registro B = new Registro();
+            B.DeleteSubKey("LicenciaActiva");
             
         }
     }
--- Licencia/Licencia/Registro.cs
+++ Licencia/Licencia/Registro.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace Licencia
 {
-    class Registro
+    public class Registro
     {
+
+        public RegistryKey key;
+        public void CreateRegister()
+        {
+            key = Registry.CurrentUser.CreateSubKey("LicenciaWindows10");
+            key.CreateSubKey("LicenciaActiva");
+        }
+
+        public void ReadSubKey(string idValue)
+        {
+            RegistryKey sk1 = key.OpenSubKey("LicenciaActiva",true);
+            sk1.SetValue("ID", idValue);
+            sk1.SetValue("Software", "AgendaApp");
+            sk1.SetValue("Version", "1.2.21");
+        }
+
+        public void DeleteSubKey(string subkey)
+        {
+            key.DeleteSubKey(subkey);
+        }
+        
     }
 }
