8da64defc43e78e7a92e3c828aa3957d3137fc70
Eldar <Eldar@DESKTOP-CTIDNPU>
2023-03-03 23:12:00 -0700

    Agregado programa simulacion de numeros psuedoAleatorios

---
+++ Generacion de numeros  pseudoaleatorios/Generacion de numeros  pseudoaleatorios/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generacion_de_numeros__pseudoaleatorios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView.Rows.Clear();
+                GeneracionDeNumeros generacionDeNumeros = new GeneracionDeNumeros();
+                Dictionary<string, string> alreadyIs = new Dictionary<string, string>();
+
+                string semilla = tbSemilla.Text;
+                string eVal = tbE.Text;
+                alreadyIs.Add(semilla, "0");
+
+                if (!generacionDeNumeros.ValidarSemilla(semilla))
+                {
+                    MessageBox.Show("La semilla debe ser de 4 digitos numericos :(");
+                    return;
+                }
+                if (!generacionDeNumeros.ValidarE(eVal))
+                {
+                    MessageBox.Show("La ´´e´´ debe ser un numero par y mayor a 8");
+                    return;
+                }
+                if(tbIteracion.Text == "")
+                {
+                    MessageBox.Show("La iteracion debe contener un numero entero");
+                    return;
+                }
+
+
+                int iteracion = int.Parse(tbIteracion.Text);
+                int i = 1;
+
+                while (iteracion >= i)
+                {
+                    string numero = generacionDeNumeros.CalcularCuadrado(semilla, eVal);
+                    string nuevaSemilla = generacionDeNumeros.CalcularNuevaSemilla(numero, eVal);
+                    string ri = generacionDeNumeros.CalcularRi(nuevaSemilla);
+
+                    dataGridView.Rows.Add(i, numero, nuevaSemilla, ri);
+
+                    if (alreadyIs.ContainsKey(nuevaSemilla))
+                    {
+                        MessageBox.Show(alreadyIs[nuevaSemilla]);
+                        return;
+                    }
+
+                    alreadyIs.Add(nuevaSemilla, i.ToString());
+                    semilla = nuevaSemilla;
+                    i++;
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+
+        }
+    }
+}
---
+++ Generacion de numeros  pseudoaleatorios/Generacion de numeros  pseudoaleatorios/GeneracionDeNumeros.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generacion_de_numeros__pseudoaleatorios
+{
+    class GeneracionDeNumeros
+    {
+       
+
+        public bool ValidarSemilla(string semilla)
+        {
+
+            if(semilla.Length != 4)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidarE(string e)
+        {
+            if(int.Parse(e) % 2 == 0 && int.Parse(e) >= 8)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public string CalcularCuadrado(string semilla,string e)
+        {
+            
+            string numeroText = Math.Pow(double.Parse(semilla), 2).ToString();
+
+            if (numeroText.Length < double.Parse(e))
+            {
+                while(numeroText.Length < double.Parse(e))
+                {
+                    numeroText = "0" + numeroText;
+                }
+
+                return numeroText;
+            }
+            return numeroText;
+        }
+
+
+        public string CalcularNuevaSemilla(string numero,string e)
+        {
+            int numeroLenght = numero.Length;
+
+            int indiceInicial = (numeroLenght - 4) / 2;
+
+            string solucion = numero.Substring(indiceInicial, 4);
+
+            return solucion;
+        }
+
+        public string CalcularRi(string semilla)
+        {
+            float solucion = float.Parse(semilla) / 10000;
+
+            return solucion.ToString();
+        }
+
+    }
+}
